f00b708b0cf541588552b268e4e71bcef21e1076
yuuxeun <229781766@qq.com>
2020-03-10 01:03:50 +0800

    sync Ver1.0

---
+++ README.md
@@ -0,0 +1,24 @@
+# `酸酸乳`
+
+### `相关说明`
+
+本项目只是对高分辨率缩放做了做了适配和下面提到的两个版本的其他区别不大
+
+并且只有.Net4.0版本
+
+VS2019重新编译
+
+参考版本链接：
+
+> 1. [https://github.com/shadowsocksrr/shadowsocksr-csharp/tree/master/shadowsocks-csharp](https://github.com/shadowsocksrr/shadowsocksr-csharp/tree/master/shadowsocks-csharp)
+>
+> 2. [https://github.com/Anankke/SSRR-Windows](https://github.com/Anankke/SSRR-Windows)
+
+### `环境`
+
+.Net 4.0+
+
+无[360/2345/百度]全家桶请使用[Akkariiin](https://github.com/Anankke/SSRR-Windows)
+
+[360/2345/百度]全家桶用户请使用[shadowsocksrr](https://github.com/shadowsocksrr/shadowsocksr-csharp)
+
---
+++ shadowsocks-csharp/View/InputPassword.cs
@@ -0,0 +1,38 @@
+﻿using Shadowsocks.Controller;
+using Shadowsocks.Properties;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Shadowsocks.View
+{
+    public partial class InputPassword : Form
+    {
+        public string password;
+
+        public InputPassword()
+        {
+            InitializeComponent();
+            this.Icon = Icon.FromHandle(Resources.ssw128.GetHicon());
+            this.Text = I18N.GetString("InputPassword");
+            label_info.Text = I18N.GetString(label_info.Text);
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            password = textPassword.Text;
+            this.DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void InputPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                password = textPassword.Text;
+                this.DialogResult = DialogResult.OK;
+                Close();
+            }
+        }
+    }
+}
